949e6160e2241eefaa7122b068d12c8dcb7febc4
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 10:40:57 -0800

    Add data annotations to ViewModel

--- EcomProject-JimmyRebecca/Models/ViewModels/RegisterAccount.cs
+++ EcomProject-JimmyRebecca/Models/ViewModels/RegisterAccount.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcomProject_JimmyRebecca.Models.ViewModels
 {
     public class RegisterAccount
     {
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        public DateTime AccountCreation { get; set; }
+
+        public DateTime AccountCreation { get; set; } = DateTime.Now;
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The passwords don't match!")]
+        [Display(Name = "Confirm Password")]
         public string PaswordConfirmation { get; set; }
     }
 }
