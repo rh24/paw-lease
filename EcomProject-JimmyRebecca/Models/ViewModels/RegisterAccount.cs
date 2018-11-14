4f21c56c1c0abefe05e12ed8967864e2b1c8668e
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 10:37:20 -0800

    Add props without validations

--- EcomProject-JimmyRebecca/Models/ViewModels/RegisterAccount.cs
+++ EcomProject-JimmyRebecca/Models/ViewModels/RegisterAccount.cs
@@ -7,5 +7,12 @@ namespace EcomProject_JimmyRebecca.Models.ViewModels
 {
     public class RegisterAccount
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime AccountCreation { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public string PaswordConfirmation { get; set; }
     }
 }
