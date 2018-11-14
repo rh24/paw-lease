de57228f6c79f081c6d94ecfae136bbc16ce4778
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 11:08:32 -0800

    Set identity services to props in constructor

--- EcomProject-JimmyRebecca/Controllers/AccountController.cs
+++ EcomProject-JimmyRebecca/Controllers/AccountController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using EcomProject_JimmyRebecca.Models;
 
 namespace EcomProject_JimmyRebecca.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
         }
