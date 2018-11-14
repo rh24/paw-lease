0afab1cc1cefd3adf249e304ecd60e0614d01ed0
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-14 11:03:47 -0800

    Inject configuration into Startup constructor

--- EcomProject-JimmyRebecca/Startup.cs
+++ EcomProject-JimmyRebecca/Startup.cs
@@ -6,11 +6,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EcomProject_JimmyRebecca
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,6 +35,9 @@ namespace EcomProject_JimmyRebecca
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
+            app.UseStaticFiles();
 
             app.UseMvc(routes =>
                 routes.MapRoute(
