e4001668c2d54a509b6fdd96416e6a9a197d5c3c
jimmyn123 <jimmyn123@gmail.com>
2018-11-14 14:53:06 -0800

    added model, added db context

---
+++ EcomProject-JimmyRebecca/Data/ProductDBContext.cs
@@ -0,0 +1,18 @@
+﻿using EcomProject_JimmyRebecca.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomProject_JimmyRebecca.Data
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options)
+        {
+        }
+
+        DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EcomProject-JimmyRebecca/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomProject_JimmyRebecca.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal SuggestedDonation { get; set; }
+    }
+}
