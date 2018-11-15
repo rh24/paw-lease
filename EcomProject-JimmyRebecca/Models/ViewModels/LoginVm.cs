460a8b5653ea1f5df1c74689cdfb024d7797d0c6
jimmyn123 <jimmyn123@gmail.com>
2018-11-15 12:05:12 -0800

    created view model

---
+++ EcomProject-JimmyRebecca/Models/ViewModels/LoginVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomProject_JimmyRebecca.Models.ViewModels
+{
+    public class LoginVm
+    {
+        // Added View Model
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Enter Your Username or Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
