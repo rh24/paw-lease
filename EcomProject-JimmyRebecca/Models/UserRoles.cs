2f7beb3726d43632de621fa3787b2921cde86b9a
jimmyn123 <jimmyn123@gmail.com>
2018-11-19 15:56:45 -0800

    added admin to 3 users with specific emails

---
+++ EcomProject-JimmyRebecca/Models/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomProject_JimmyRebecca.Models
+{
+    public class UserRoles
+    {
+        public const string Member = "Member";
+        public const string Admin = "Administrator";
+    }
+}
+}
