1a87e4313233b60f11a215f33f7ca3f08603591c
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 14:06:05 -0800

    Only display product in index if it is not a cat

--- EcomProject-JimmyRebecca/Controllers/CatAdoptionController.cs
+++ EcomProject-JimmyRebecca/Controllers/CatAdoptionController.cs
@@ -6,7 +6,20 @@ namespace EcomProject_JimmyRebecca.Controllers
     [Authorize(Policy = "CatLover")]
     public class CatAdoptionController : Controller
     {
+        /// <summary>
+        /// This page will show all the cats up for adoption.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This will be the action that gets the checkout page for adopting a cat.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult CheckoutPage()
         {
             return View();
         }
