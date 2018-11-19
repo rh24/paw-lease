57aeaa41e939034f9d440da8197539a41f6aea35
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 10:36:20 -0800

    Add non-implemented override of asnyc handler method

--- EcomProject-JimmyRebecca/Models/Handlers/LovesCatsHandler.cs
+++ EcomProject-JimmyRebecca/Models/Handlers/LovesCatsHandler.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Authorization;
 using System.Threading.Tasks;
 
 namespace EcomProject_JimmyRebecca.Models.Handlers
 {
-    public class LovesCatsHandler
+    public class LovesCatsHandler : AuthorizationHandler<LovesCatsRequirement>
     {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, LovesCatsRequirement requirement)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
