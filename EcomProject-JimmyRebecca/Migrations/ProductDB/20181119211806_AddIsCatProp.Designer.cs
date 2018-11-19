7df9259176b9e48907a5817f819e2be1b717c4ef
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 13:21:53 -0800

    Update connection strings

--- EcomProject-JimmyRebecca/Migrations/ProductDB/20181119211806_AddIsCatProp.Designer.cs
+++ EcomProject-JimmyRebecca/Migrations/ProductDB/20181119211806_AddIsCatProp.Designer.cs
@@ -3,14 +3,16 @@ using EcomProject_JimmyRebecca.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EcomProject_JimmyRebecca.Migrations.ProductDB
 {
     [DbContext(typeof(ProductDBContext))]
-    partial class ProductDBContextModelSnapshot : ModelSnapshot
+    [Migration("20181119211806_AddIsCatProp")]
+    partial class AddIsCatProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
