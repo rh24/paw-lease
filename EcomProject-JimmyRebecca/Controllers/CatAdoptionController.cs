6955abf98a6fa36202664b6d59334b917e66c1fc
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 14:02:51 -0800

    Add CatAdoptionController with authorization policy

---
+++ EcomProject-JimmyRebecca/Controllers/CatAdoptionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcomProject_JimmyRebecca.Controllers
+{
+    [Authorize(Policy = "CatLover")]
+    public class CatAdoptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
