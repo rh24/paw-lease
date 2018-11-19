15531a834ceb1e6137ae6585457e13883dbf76a6
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 10:27:30 -0800

    Add prop i love cats and set in constructor

--- EcomProject-JimmyRebecca/Models/Handlers/LovesCatsRequirement.cs
+++ EcomProject-JimmyRebecca/Models/Handlers/LovesCatsRequirement.cs
@@ -4,5 +4,11 @@ namespace EcomProject_JimmyRebecca.Models.Handlers
 {
     public class LovesCatsRequirement : IAuthorizationRequirement
     {
+        public bool ILoveCats { get; set; }
+
+        public LovesCatsRequirement(bool iLoveCats)
+        {
+            ILoveCats = iLoveCats;
+        }
     }
 }
