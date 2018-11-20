1c35e3a4c2437e3b34113d33b5069b01a85e3067
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 11:02:29 -0800

    Finish interface for ICart

---
+++ EcomProject-JimmyRebecca/Models/Interfaces/ICheckout.cs
@@ -0,0 +1,9 @@
+﻿namespace EcomProject_JimmyRebecca.Models.Interfaces
+{
+    /// <summary>
+    /// In the future, we may want to consider having a checkout interface that the user and/or cart implements.
+    /// </summary>
+    interface ICheckout
+    {
+    }
+}
