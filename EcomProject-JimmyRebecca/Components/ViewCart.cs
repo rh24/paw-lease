d01ecc0e2119527b07e9f86fea2aa50b131f634d
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 13:10:07 -0800

    Revert "Add documentation for View Component"
    This reverts commit 25705fcba715ee89491a983af0db181cb6a9d1f5.

--- EcomProject-JimmyRebecca/Components/ViewCart.cs
+++ EcomProject-JimmyRebecca/Components/ViewCart.cs
@@ -10,21 +10,11 @@ namespace EcomProject_JimmyRebecca.Components
     {
         private readonly ProductDBContext _context;
 
-        /// <summary>
-        /// Constructor injection
-        /// </summary>
-        /// <param name="context">ProductDBContext</param>
         public ViewCart(ProductDBContext context)
         {
             _context = context;
         }
 
-        /// <summary>
-        /// Method to query the db context for LineItem objects where the cart ID and Product ID is equal to the values passed in. The arguments will be provided by the view that invokes this method.
-        /// </summary>
-        /// <param name="cartID">Cart ID</param>
-        /// <param name="productID">Product ID</param>
-        /// <returns>View that calls this method</returns>
         public async Task<IViewComponentResult> InvokeAsync(int cartID, int productID)
         {
             var lineItems = await _context.LineItems.Where(li => li.Product.ID == productID && li.Cart.ID == cartID).ToListAsync();
