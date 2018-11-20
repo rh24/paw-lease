b44e0a59740f06e5e19467b4ab0d29caa76122dc
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-19 19:27:23 -0800

    Add empty test project and additional documentation in AccountController

---
+++ Sprint1_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Sprint1_Tests
+{
+    /// <summary>
+    /// Test getters and setters for all models
+    /// </summary>
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
