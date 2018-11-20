6c3495c7ff1142bad441ceb61264fffb48e98493
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 11:07:09 -0800

    Add skeleton of interface methods in LineItem and Cart services

--- EcomProject-JimmyRebecca/Models/Services/CartService.cs
+++ EcomProject-JimmyRebecca/Models/Services/CartService.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using EcomProject_JimmyRebecca.Models.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EcomProject_JimmyRebecca.Models.Services
 {
-    public class CartService
+    public class CartService : ICart
     {
+        public Task CreateCart(Cart product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCart(Cart product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cart> GetCart(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Cart>> GetCarts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCart(Cart product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- EcomProject-JimmyRebecca/Models/Services/LineItemService.cs
+++ EcomProject-JimmyRebecca/Models/Services/LineItemService.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using EcomProject_JimmyRebecca.Models.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EcomProject_JimmyRebecca.Models.Services
 {
-    public class LineItemService
+    public class LineItemService : ILineItem
     {
+        public Task CreateLineItem(LineItem product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteLineItem(LineItem product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LineItem> GetLineItem(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LineItem>> GetLineItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateLineItem(LineItem product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
