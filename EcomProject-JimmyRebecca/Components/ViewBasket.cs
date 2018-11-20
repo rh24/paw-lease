42c428b41b59f3b6cf050bd5369db6153a09076b
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 12:29:28 -0800

    Add constructor injection in ViewBasket view component and implement ViewComponent interface

---
+++ EcomProject-JimmyRebecca/Components/ViewBasket.cs
@@ -0,0 +1,21 @@
+﻿using EcomProject_JimmyRebecca.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EcomProject_JimmyRebecca.Components
+{
+    public class ViewBasket : ViewComponent
+    {
+        private readonly ProductDBContext _context;
+
+        public ViewBasket(ProductDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int number)
+        {
+
+        }
+    }
+}
---
+++ EcomProject-JimmyRebecca/Migrations/ProductDB/20181120202347_UpdateDatabaseSchema.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcomProject_JimmyRebecca.Migrations.ProductDB
+{
+    public partial class UpdateDatabaseSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    NormalizedUserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    AccountCreation = table.Column<DateTime>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    LovesCats = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserID = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Carts_ApplicationUser_UserId",
+                        column: x => x.UserId,
+                        principalTable: "ApplicationUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LineItems",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProductID = table.Column<int>(nullable: false),
+                    CartID = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LineItems", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_LineItems_Carts_CartID",
+                        column: x => x.CartID,
+                        principalTable: "Carts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LineItems_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "IsCat",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "IsCat",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "IsCat",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "IsCat",
+                value: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineItems_CartID",
+                table: "LineItems",
+                column: "CartID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineItems_ProductID",
+                table: "LineItems",
+                column: "ProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LineItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUser");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "IsCat",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "IsCat",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "IsCat",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "IsCat",
+                value: false);
+        }
+    }
+}
