66b009c536ae55cd371b943146dd06d7c51cfc27
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 11:03:43 -0800

    Add interface for LineItems

--- EcomProject-JimmyRebecca/Models/Interfaces/ILineItem.cs
+++ EcomProject-JimmyRebecca/Models/Interfaces/ILineItem.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace EcomProject_JimmyRebecca.Models.Interfaces
 {
     interface ILineItem
     {
+        // CRUD operations
+        // interface for LineItem
+        Task CreateLineItem(LineItem product);
+        Task UpdateLineItem(LineItem product);
+        Task DeleteLineItem(LineItem product);
+        Task<LineItem> GetLineItem(int? id);
+        Task<IEnumerable<LineItem>> GetLineItems();
     }
 }
