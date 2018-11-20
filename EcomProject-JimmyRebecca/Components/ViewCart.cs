e7b5c709ec00a70551c79d91f819845e4dde7066
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-20 12:52:02 -0800

    Finish ViewCart view component

--- EcomProject-JimmyRebecca/Components/ViewCart.cs
+++ EcomProject-JimmyRebecca/Components/ViewCart.cs
@@ -1,5 +1,7 @@
 ﻿using EcomProject_JimmyRebecca.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EcomProject_JimmyRebecca.Components
@@ -13,9 +15,11 @@ namespace EcomProject_JimmyRebecca.Components
             _context = context;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(int number)
+        public async Task<IViewComponentResult> InvokeAsync(int cartID, int productID)
         {
+            var lineItems = await _context.LineItems.Where(li => li.Product.ID == productID && li.Cart.ID == cartID).ToListAsync();
 
+            return View(lineItems);
         }
     }
 }
