9de34d6214597662cd4c27cb5e073b9f72566287
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-28 10:00:14 -0800

    Add error view for 404

---
+++ EcomProject-JimmyRebecca/Controllers/HttpErrorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcomProject_JimmyRebecca.Controllers
+{
+    public class HttpErrorsController : Controller
+    {
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
