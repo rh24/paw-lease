045933e9f7275f831ffbfb9ea416a1dd1dc0f85b
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-29 11:20:02 -0800

    Inject necessary services into checkout controller in order to grab user's cart

--- EcomProject-JimmyRebecca/Controllers/CheckoutController.cs
+++ EcomProject-JimmyRebecca/Controllers/CheckoutController.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcomProject_JimmyRebecca.Models;
+using EcomProject_JimmyRebecca.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcomProject_JimmyRebecca.Controllers
 {
     public class CheckoutController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private readonly ICart _context;
+
+        public CheckoutController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ICart context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        /// <summary>
+        /// This method brings in all of the items in the user's cart that they have purchased and passes it along to the Receipt.cshtml view.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Receipt()
         {
             return View();
