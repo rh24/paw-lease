6c5edeea19828880fe790e6633be6d58558f401d
Rebecca Hong <rebeccayhong@gmail.com>
2018-11-29 11:17:10 -0800

    Add checkout controller and receipt action

---
+++ EcomProject-JimmyRebecca/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcomProject_JimmyRebecca.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Receipt()
+        {
+            return View();
+        }
+    }
+}
