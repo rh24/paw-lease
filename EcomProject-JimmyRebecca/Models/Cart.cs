292cfef8694f1d16800a73341316111ca2dcf0c0
Jimmy Nguyen <jimmyn123@gmail.com>
2018-11-30 18:19:50 -0800

    fixed the cart! It now adds a new one when you check out!

--- EcomProject-JimmyRebecca/Models/Cart.cs
+++ EcomProject-JimmyRebecca/Models/Cart.cs
@@ -7,13 +7,13 @@ namespace EcomProject_JimmyRebecca.Models
     {
         // Primary key
         public int ID { get; set; }
+        public string UserID { get; set; }
         public bool OrderFulfilled { get; set; }
 
         // Navigation props:
         public ICollection<LineItem> LineItems { get; set; }
 
         // A cart belongs to a user
-        [ForeignKey("UserId")]
         public ApplicationUser User { get; set; }
     }
 }
