7d1396de00c32a0f70fd43743092c86de53894c4
jimmyn123 <jimmyn123@gmail.com>
2018-12-03 12:02:47 -0800

    changed model to include URLs

--- EcomProject-JimmyRebecca/Migrations/20181203193855_initial-identity.Designer.cs
+++ EcomProject-JimmyRebecca/Migrations/20181203193855_initial-identity.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcomProject_JimmyRebecca.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20181201184723_initial-identity")]
+    [Migration("20181203193855_initial-identity")]
     partial class initialidentity
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
+                .HasAnnotation("ProductVersion", "2.1.3-rtm-32065")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -136,6 +136,8 @@ namespace EcomProject_JimmyRebecca.Migrations
                         .IsRequired();
 
                     b.Property<decimal>("SuggestedDonation");
+
+                    b.Property<string>("image_url");
 
                     b.HasKey("ID");
 
