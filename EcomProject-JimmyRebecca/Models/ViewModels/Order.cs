8c251cd63d124da08ed7db4953fa65058736abbc
Rebecca Hong <rebeccayhong@gmail.com>
2018-12-06 12:37:15 -0800

    Add order view model

---
+++ EcomProject-JimmyRebecca/Models/ViewModels/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EcomProject_JimmyRebecca.Models.ViewModels
+{
+    public class Order
+    {
+        public string CreditCardNumber { get; set; }
+        public int CartID { get; set; }
+
+        // Shadow Property
+        public Cart Cart { get; set; }
+
+        public ICollection<LineItem> OrderItems { get; set; }
+
+        // User information to capture
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
